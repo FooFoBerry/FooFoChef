7df0cf02b626667f9884b19fd0a1e2dc1b7e068b
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-16 09:37:55 -0700

    Delete metadata.rb

--- site-cookbooks/FooFoBerry/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'FooFoBerry'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures FooFoBerry'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
