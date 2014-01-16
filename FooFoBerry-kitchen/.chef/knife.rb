38a33d5605624eb37557fd2154cf63a6526e86fa
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-15 19:56:48 -0700

    digital ocean api key and id added

--- FooFoBerry-kitchen/.chef/knife.rb
+++ FooFoBerry-kitchen/.chef/knife.rb
@@ -6,3 +6,7 @@ data_bag_path    "data_bags"
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
+
+cookbook_path [ './cookbooks' ]
+knife[:digital_ocean_client_id] = '192.241.137.8'
+knife[:digital_ocean_api_key]   = 'root@192.241.137.8'
